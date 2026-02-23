df83e4f9ac6a6974336380b08805412234689c85
root <kushagra.17992@sakec.ac.in>
2026-02-23 08:51:37 +0430

    started finished structure started union

---
+++ 5_struct_uni_bit/codecracker.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+typedef struct {
+	const char *description;
+	float value;
+} swag;
+
+typedef struct {
+	swag *swag;
+	const char *sequence;
+}combination;
+
+typedef struct {
+	combination numbers;
+	const char *make;
+} safe;
+
+
+swag gold = {"GOLD", 1000000.0};
+combination numbers = {&gold, "6502"};
+safe s = {numbers, "RAMACON250"}
+
+goal to access: GOLD
+
+soln: 
+s.numbers.swag->gold
+
+
