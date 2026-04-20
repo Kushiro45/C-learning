1483d8a3fd9e00ea84e2bc5da9c61dc73645fc80
root <kushagra.17992@sakec.ac.in>
2026-04-20 16:38:39 -0400

    starting with signals

--- 10_Interprocess_Communication/browser_better.c
+++ 10_Interprocess_Communication/browser_better.c
@@ -6,8 +6,19 @@
 
 
 void error(char *msg){
-	fprintf(stderr, )
+	fprintf(stderr, "%s:%s", msg, strerror(errno));
 }
+
+void open_url(char *url){
+	char launch[255];
+	sprintf(launch, "cmd /c start %s", url);
+	system(launch);
+	sprintf(launch, "x-www-browser '%s' &", url);
+	system(launch);
+	sprintf(launch, "open '%s'", url);
+	system(launch);
+}
+
 
 int main(int argc, char *argv[]){
 	char *phrase = argv[1];
@@ -23,9 +34,17 @@ int main(int argc, char *argv[]){
 	if(!pid){
 		dup2(fd[1], 1);
 		close(fd[0]);
-		if (execle("/usr/bin/python", "/usr/bin/python", "./rssgossip.py", "-u", phrase, NULL, vars) == -1){
+		if (execle("/usr/bin/python", "/usr/bin/python", "./rssfeed.py", "-u", phrase, NULL, vars) == -1){
 			error("cant run script");
 		}
 	}
+	dup2(fd[0], 0);
+	close(fd[1]);
+	char line[255];
+	while (fgets(line, 255, stdin)){
+		if(line[0] == '\t')
+			open_url(line + 1 );
+	}
+	return 0;
 
 }
--- 10_Interprocess_Communication/hello.c
+++ 10_Interprocess_Communication/hello.c
@@ -3,13 +3,14 @@
 #include <unistd.h>
 #include <errno.h>
 #include <string.h>
+#include <sys/wait.h>
+
 
 void error(char *msg){
 	fprintf(stderr, "%s: %s", msg, strerror(errno));
 }
 
 int main(int argc, char *argv[]){
-
 	char *phrase = argv[1];
 	char *vars[] = {"RSS_FEED=https://research.checkpoint.com/feed/", NULL};
 	FILE *f = fopen("stories.txt", "w");
@@ -17,6 +18,10 @@ int main(int argc, char *argv[]){
 		error("Cant open stories.txt");
 	}
 	pid_t pid = fork();
+	int pid_status;
+	if (waitpid(pid, &pid_status, 0) == -1){
+		error("Error waiting for child process");
+	}
 	if(pid == -1){
 		if(dup2(fileno(f), 1) == -1){
 			error("Cant redirect to Standard Output");
